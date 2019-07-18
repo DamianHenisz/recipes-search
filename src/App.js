49aeee41256e7ef9ffa62057087d6a34b37cc46d
Damian_Henisz <damianh1293@gmail.com>
2019-07-18 23:42:15 +0200

    Added dependencies bootstrap and react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import { Provider, connect } from "react-redux";
+//import store from "./store/configureStore";
+import SearchRecipesComponent from "./components/SearchRecipesComponent";
+import ListDropdownComponent from "./components/ListDropdownComponent";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      // <Provider //store={store}>
+      <Container className="App-Container">
+        <Row>
+          <Col sm={4}>
+            <ListDropdownComponent />
+          </Col>
+          <Col sm={8}>
+            <SearchRecipesComponent />
+          </Col>
+        </Row>
+        <p>TEST</p>
+        <p>TEST</p>
+        <p>TEST</p>
+        <p>TEST</p>
+        <p>TEST</p>
+        <p>TEST</p>
+
+        <p>TEST</p>
+        <p>TEST</p>
+        <p>TEST</p>
+
+        <p>TEST</p>
+        <p>TEST</p>
+        <p>TEST</p>
+
+        <p>TEST</p>
+      </Container>
+
+      // </Provider>
+    );
+  }
 }
-
 export default App;
