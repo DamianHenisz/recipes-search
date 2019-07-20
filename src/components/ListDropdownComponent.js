1c6e8d45a0947ba8fae6c34450eb19da04650b00
Damian_Henisz <damianh1293@gmail.com>
2019-07-20 18:29:38 +0200

    Added ListDropdownComponent

--- src/components/ListDropdownComponent.js
+++ src/components/ListDropdownComponent.js
@@ -9,15 +9,13 @@ class ListDropdownComponent extends Component {
 
   render() {
     return (
-      <Form.Group>
-        <Form.Control as="select">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </Form.Control>
-      </Form.Group>
+      <Form.Control as="select">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+      </Form.Control>
     );
   }
 }
