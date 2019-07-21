647c50b41d96e44d5a0c820d5e50f1a9bf9d2990
Damian_Henisz <damianh1293@gmail.com>
2019-07-21 20:12:34 +0200

    Added styles in RecipesItemComponent

--- src/components/RecipesItemComponent.js
+++ src/components/RecipesItemComponent.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
+import Media from "react-bootstrap/Media";
 import "../App.css";
 class RecipesItemComponent extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="recipesListComponent__row">
-        <span>
-          <img src={this.props.thumbnail} alt="img" />
-          <h3> {this.props.title}</h3>
-          <p>{this.props.ingredients}</p>
-          <a href={this.props.href}>{this.props.href}</a>
-        </span>
+        <Media>
+          <img width={200} height={200} className="align-self-center mr-3" src={this.props.thumbnail} alt="img" />
+          <Media.Body>
+            <h3> {this.props.title}</h3>
+            <h5>Ingredients:</h5>
+            <p>{this.props.ingredients}</p>
+            <a href={this.props.href}>{this.props.href}</a>
+          </Media.Body>
+        </Media>
       </div>
     );
   }
